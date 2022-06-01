85bbf25e86ec0a67a7141c11f2ffc09e887e0452
becode <alois.zweber@gmail.com>
2022-06-01 10:36:35 +0200

    Finishing exercice with countries from an API

---
+++ cours-react-1/src/pages/About.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1> A propos </h1>
+            <br />
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum rerum eius fugit officiis sapiente explicabo rem magni.
+                Eaque quos totam a suscipit ullam culpa nesciunt illum.
+                Saepe facilis aspernatur illo vitae dolores ipsum alias voluptates nulla, dolorem a vel.
+                Mollitia, dolorem ut.
+                Quam sed asperiores molestias rem illum minima fuga sit vitae cumque at nemo laboriosam,
+                officiis numquam assumenda tempora ad recusandae!
+                Dicta debitis quis tempore doloremque quasi enim quae,
+                eum ipsum quia magni aliquam ducimus rem placeat iste explicabo modi ullam dolor distinctio a repellendus hic!
+                Illo totam cupiditate ratione sint eaque fugit veritatis similique ullam iste possimus nemo laudantium corporis
+                vero recusandae nihil ex enim dignissimos vel quam inventore tenetur, sed sit placeat repellendus?
+                Corrupti ipsam sit facere accusantium consequuntur facilis deleniti doloribus?
+            </p>
+            <br />
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Atque dolorem enim beatae saepe repudiandae!
+                Quidem maiores recusandae veniam cum quis ullam, dignissimos tenetur!
+                Eos iure rerum excepturi incidunt,
+                tempore culpa tenetur necessitatibus quidem provident odit magnam ab iusto ut doloribus
+                molestiae aliquam corrupti accusamus omnis aperiam deserunt veniam.
+                Quidem ullam eum quas alias aperiam,
+                esse placeat eligendi earum voluptatum ut praesentium suscipit ab debitis repellendus!
+                Vitae ad id, iste sit inventore nemo voluptate quia quis nihil, beatae laudantium saepe
+                velit maxime pariatur. Delectus ratione hic perspiciatis sint dolorem maiores explicabo fugit,
+                quo unde, fugiat est in voluptatibus quisquam deleniti commodi!
+            </p>
+        </div>
+    );
+};
+
+export default About;
